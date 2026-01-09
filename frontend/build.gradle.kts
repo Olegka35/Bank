d8d7df9e5d7c07290c1a3c409f93b6ade751cb63
Oleg <olegka35@gmail.com>
2026-01-09 22:31:18 +0300

    Frontend connection to config server

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 }
 
 tasks.test {
