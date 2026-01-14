162532f51b6907cbb98f0d7839a82e1984d17310
Oleg <olegka35@gmail.com>
2026-01-14 23:38:34 +0300

    Enable method security

--- account-service/src/main/java/com/tarasov/bank/account/configuration/SecurityConfig.java
+++ account-service/src/main/java/com/tarasov/bank/account/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.tarasov.bank.account.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
