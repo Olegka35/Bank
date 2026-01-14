13dab68543124960ca724cfb6f4641f053e594ee
Oleg <olegka35@gmail.com>
2026-01-15 01:17:43 +0300

    Cash, transfer and notification services init

---
+++ cash-service/src/main/java/com/tarasov/bank/service/controller/CashServiceController.java
@@ -0,0 +1,15 @@
+package com.tarasov.bank.service.controller;
+
+
+import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class CashServiceController {
+
+}
---
+++ notification-service/src/main/java/com/tarasov/bank/transfer/controller/NotificationServiceController.java
@@ -0,0 +1,11 @@
+package com.tarasov.bank.transfer.controller;
+
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class NotificationServiceController {
+
+}
