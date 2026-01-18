d14a067d31200485274f766cd5c2639a7bcef36b
Oleg <olegka35@gmail.com>
2026-01-18 15:33:54 +0300

    remove extra import

--- account-service/src/main/java/com/tarasov/bank/account/service/AccountServiceImpl.java
+++ account-service/src/main/java/com/tarasov/bank/account/service/AccountServiceImpl.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.NoSuchElementException;
